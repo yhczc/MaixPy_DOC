30aeffe5ec38b1625c62f877394f0515deeea6c7
Neucrack <czd666666@gmail.com>
2019-01-11 14:42:19 +0800

    add local serve tool

--- serve.sh
+++ serve.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-echo "Build now"
-gitbook build
-cp -f index.html _book/index.html
-cp -f assets/favicon.ico _book/gitbook/images/favicon.ico
-cp -f assets/icon_sipeed.png _book/gitbook/images/apple-touch-icon-precomposed-152.png
-echo "Build end"
 
-echo "Run server now at http://localhost:4000"
-cd _book && python -m SimpleHTTPServer 4000
+function cancel()
+{
+    sub=`ps -ef|grep "serve_demon.sh"|grep -v grep|awk '{print $2}'`
+    if [[ "x$sub" != "x" ]]; then
+        `ps -ef|grep "serve_demon.sh"|grep -v grep|awk '{print $2}'|xargs kill -9`
+    fi
+    rm -rf serve.log
+    exit 0
+}
+
+trap cancel SIGINT SIGTERM SIGQUIT
+
+
+echo "" > serve.log
+./serve_demon.sh &
+gitbook serve | tee serve.log
 
