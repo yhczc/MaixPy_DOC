2d2fe61e7782398516bab0c9cb9f8d0a1ff4ac37
Neucrack <czd666666@gmail.com>
2019-01-15 11:35:27 +0800

    multiple version support

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,31 @@ set -o errexit -o nounset
 rev=$(git rev-parse --short HEAD)
 
 cp -f index.html build/index.html
-
 cp -f assets/favicon.ico build/gitbook/images/favicon.ico
-
 cp -f assets/icon_sipeed.png build/gitbook/images/apple-touch-icon-precomposed-152.png
+
+tmp=`cat .git/HEAD|awk '{print $2}'`
+curr_branch=${tmp##*/}
+git clone -b gh-pages https://github.com/Ai-Thinker-Open/GPRS_C_SDK_DOC.git ./old
+if [[ '$curr_branch' == 'master' ]]; then
+    if [[ -d './old/dev' ]]; then
+        cp -r ./old/dev ./build/
+    fi
+    cd ./old
+    v_folder=`find ./ -maxdepth 1 -name 'v*.*'`
+    if [[ 'x$v_folder' != 'x' ]]; then
+        cp -r $v_folder ../build/
+    fi
+    cd ..
+elif [[ '$curr_branch' == 'dev' ]]; then
+    cp -r ./build/* ./old/dev
+    rm -rf ./build
+    mv ./old ./build
+else
+    cp -r ./build/* ./old/$curr_branch
+    rm -rf ./build
+    mv ./old ./build
+fi
 
 cd build
 
